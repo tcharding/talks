14198dc51fffff39b70906416a851845bb9a767e
Tobin C. Harding <me@tobin.cc>
2023-03-06 14:33:20 +1100

    Add num crate
    As part of the "Advanced Testing in Rust" talk; add a `num` crate to
    demo Rust testing.

---
+++ advanced-testing-in-rust/num/src/lib.rs
@@ -0,0 +1,107 @@
+//! Number wrapper type.
+//!
+//! This crate is representative of a wrapper type for an integer that implements some desired
+//! abstraction.
+//!
+//! Used to demonstrate testing methodology, the API and logic may at times be contrived.
+
+use std::fmt;
+
+/// A signed 32 bit integer type.
+pub struct Num(i32);
+
+impl Num {
+    /// Constructs a new `Num` type from a signed integer.
+    pub fn from_signed(x: i32) -> Self {
+        Num(x)
+    }
+
+    /// Constructs a new `Num` type from an unsigned integer.
+    pub fn from_unsigned(x: u32) -> Result<Self, Error> {
+        if x <= i32::MAX as u32 {
+            Ok(Num(x as i32))
+        } else {
+            Err(Error::Overflow(x))
+        }
+    }
+
+    /// Returns the value of this number as a signed integer.
+    pub fn to_signed(self) -> i32 {
+        self.0
+    }
+
+    /// Returns the value of this number as a unsigned integer.
+    ///
+    /// # Errors
+    ///
+    /// It is an error to call `to_unsigned` if this number is negative.
+    pub fn to_unsigned(self) -> Result<u32, Error> {
+        if self.0 < 0 {
+            return Err(Error::Negative);
+        }
+        self.0.try_into().map_err(|_| Error::Negative)
+    }
+
+    /// Returns the absolute value of this number.
+    pub fn abs(self) -> u32 {
+        let abs = if self.0 < 0 { 0 - self.0 } else { self.0 };
+
+        abs.try_into().expect("won't panic, int is positive")
+    }
+}
+
+impl From<i32> for Num {
+    fn from(x: i32) -> Self {
+        Num::from_signed(x)
+    }
+}
+
+impl TryFrom<u32> for Num {
+    type Error = Error;
+    fn try_from(x: u32) -> Result<Self, Self::Error> {
+        Num::from_unsigned(x)
+    }
+}
+
+/// A `Num` related error.
+#[derive(Debug)]
+pub enum Error {
+    /// Unsigned integer overflows `Num`.
+    Overflow(u32),
+    /// Unexpected negative value.
+    Negative,
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use Error::*;
+
+        match *self {
+            Overflow(_) => write!(f, "unsigned integer overflows signed number"),
+            Negative => write!(f, "unexpected negative value"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn abs_handles_positive_value() {
+        let x: i32 = 10;
+        let num = Num::from_signed(x);
+        let abs = num.abs();
+        assert_eq!(abs, x as u32)
+    }
+
+    #[test]
+    fn abs_handles_negative_value() {
+        let x: i32 = -10;
+        let num = Num::from_signed(x);
+        let abs = num.abs();
+        assert_eq!(abs, -x as u32)
+    }
+}
